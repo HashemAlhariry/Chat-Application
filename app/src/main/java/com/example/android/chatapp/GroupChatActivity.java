911865a6ff0feeff5d056729542ab8a43967a3e9
Etch <hashem_khaled33@yahoo.com>
2019-09-04 19:31:20 +0200

    Some Feature added

---
+++ app/src/main/java/com/example/android/chatapp/GroupChatActivity.java
@@ -0,0 +1,52 @@
+package com.example.android.chatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class GroupChatActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private ImageButton sendMessageButton;
+    private EditText userMessageInput;
+    private ScrollView  mScrollView;
+    private TextView displayTextMessages;
+
+    private String currentGroupName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_chat);
+
+        currentGroupName = getIntent().getExtras().get("groupName").toString();
+
+        InitializeFields();
+
+        Toast.makeText(GroupChatActivity.this,currentGroupName,Toast.LENGTH_SHORT).show();
+
+
+    }
+
+    private void InitializeFields() {
+
+        mToolbar = (Toolbar) findViewById( R.id.group_chat_bar_layout);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle(currentGroupName);
+
+        sendMessageButton = (ImageButton) findViewById(R.id.send_message_button);
+        userMessageInput =(EditText) findViewById(R.id.input_group_message);
+        displayTextMessages= (TextView)findViewById(R.id.group_chat_text_display);
+
+        mScrollView= (ScrollView) findViewById(R.id.my_scroll_view);
+    }
+
+
+}
--- app/src/main/java/com/example/android/chatapp/MainActivity.java
+++ app/src/main/java/com/example/android/chatapp/MainActivity.java
@@ -1,21 +1,33 @@
 package com.example.android.chatapp;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TableLayout;
+import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
 
+    private DatabaseReference RootRef;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -36,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         currentuser= mAuth.getCurrentUser();
+        RootRef= FirebaseDatabase.getInstance().getReference();
 
         mToolbar=(Toolbar) findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolbar);
@@ -55,10 +69,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
+
         if(currentuser == null)
         {
             sendUserToLoginActivity();
+
         }
+        else
+        {
+            VerifyUserExistance();
+        }
+
+    }
+
+    private void VerifyUserExistance()
+    {
+
+        String currentUserID= mAuth.getCurrentUser().getUid();
+        RootRef.child("Users").child(currentUserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if((dataSnapshot.child("name").exists()))
+                {
+                    Toast.makeText(MainActivity.this,"Welcome",Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    sendUserToSettingsActivity();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
     }
 
 
@@ -83,6 +131,11 @@ public class MainActivity extends AppCompatActivity {
         {
                 sendUserToSettingsActivity();
         }
+        if(item.getItemId()== R.id.main_create_group_option)
+        {
+
+            RequestNewGroup();
+        }
         if(item.getItemId()== R.id.main_find_friends_option)
         {
 
@@ -91,15 +144,83 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+
+
+    private void RequestNewGroup()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this,R.style.AlertDialog);
+        builder.setTitle("Enter Group Name : ");
+
+        final EditText groupNameField = new EditText(MainActivity.this);
+
+        groupNameField.setHint("e.g Family");
+
+        builder.setView(groupNameField);
+
+
+        builder.setPositiveButton("Create", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i)
+            {
+                String groupName = groupNameField.getText().toString();
+
+                if(TextUtils.isEmpty(groupName))
+                {
+                    Toast.makeText(MainActivity.this, "Please Write Group Name", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    CreateNewGroup(groupName);
+
+                }
+
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i)
+            {
+
+                dialogInterface.cancel();
+
+
+            }
+        });
+
+        builder.show();
+    }
+
+
+
+    private void CreateNewGroup(final String groupName)
+    {
+        RootRef.child("Groups").child(groupName).setValue("")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(MainActivity.this,groupName + "group is Created Successfully ....",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+
     private void sendUserToLoginActivity()
     {
         Intent loinIntent = new Intent(MainActivity.this,LoginActivity.class);
+        loinIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(loinIntent);
+        finish();
     }
     private void sendUserToSettingsActivity()
     {
         Intent SettingsIntent = new Intent(MainActivity.this,SettingsActivity.class);
+        SettingsIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(SettingsIntent);
+        finish();
     }
 
 
